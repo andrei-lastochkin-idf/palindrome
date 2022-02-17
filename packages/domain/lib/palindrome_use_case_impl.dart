8ba298645d13b6c75590ed21085027ad06d59b2a
andrei-lastochkin-idf <andrei.lastochkin@idfinance.com>
2022-02-17 23:06:02 +0300

    Replaced logic of palindrome definition

--- packages/domain/lib/palindrome_use_case_impl.dart
+++ packages/domain/lib/palindrome_use_case_impl.dart
@@ -5,26 +5,13 @@ import 'package:domain/palindrome_use_case.dart';
 class PalindromeUseCaseImpl implements PalindromeUseCase {
   @override
   bool isPalindrome(String rawString) {
-    bool result = false;
-
     String text = rawString.toLowerCase().trim();
+    String reversedText = text.split('').reversed.join('');
 
-    int reversedIndex = text.length - 1;
-
-    for (int index = 0; index < text.length; index++) {
-      if (text[index] == text[reversedIndex]) {
-        result = true;
-      } else {
-        result = false;
-      }
-
-      reversedIndex--;
-
-      if (reversedIndex == (text.length / 2).round() || result == false) {
-        break;
-      }
+    if (text == reversedText) {
+      return true;
+    } else {
+      return false;
     }
-
-    return result;
   }
 }
