278886656757985bd26cd1041f0aecacb581639d
andrei-lastochkin-idf <andrei.lastochkin@idfinance.com>
2022-02-21 15:20:11 +0300

    Implemented BLoC

--- packages/presentation/lib/application.dart
+++ packages/presentation/lib/application.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:domain/palindrome_use_case_impl.dart';
+import 'home/bloc/home_bloc.dart';
+import 'home/home_page.dart';
 
 class PalindromeApp extends StatelessWidget {
   const PalindromeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<HomeBloc>(
+          create: (context) => HomeBloc(useCase: PalindromeUseCaseImpl())
+            ..add(HomeInitialEvent()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(title: 'Palindrome check'),
     );
   }
 }
