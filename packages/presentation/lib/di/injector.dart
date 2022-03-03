51bba770693c9e31d435ae4d09d6fee56a099cf5
andrei-lastochkin-idf <andrei.lastochkin@idfinance.com>
2022-03-03 19:15:11 +0300

    Fixes according to comments

--- packages/presentation/lib/di/injector.dart
+++ packages/presentation/lib/di/injector.dart
@@ -5,7 +5,7 @@ import 'package:presentation/screen/home/bloc/home_bloc.dart';
 void initPresentationModule() {
   final locator = GetIt.I;
 
-  locator.registerFactory<HomeBloc>(() => HomeBloc(
-        locator.get<PalindromeUseCase>(),
-      ));
+  locator.registerFactory<HomeBloc>(
+    () => HomeBloc(locator.get<PalindromeUseCase>()),
+  );
 }
